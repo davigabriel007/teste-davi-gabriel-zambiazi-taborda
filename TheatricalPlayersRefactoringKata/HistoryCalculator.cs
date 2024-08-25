533965c14f96ac26a80d46339f07481b73ae2b21
Davi Gabriel Zambiazi Taborda <davi.taborda07@gmail.com>
2024-08-25 16:51:32 -0300

    Criação do gênero histórico e atualização do código de teste para incluir peças históricas

---
+++ TheatricalPlayersRefactoringKata/HistoryCalculator.cs
@@ -0,0 +1,22 @@
+﻿using TheatricalPlayersRefactoringKata;
+
+public class HistoryCalculator : IGenreCalculator
+{
+    public int CalculateAmount(Play play, Performance performance)
+    {
+        var tragedyCalculator = new TragedyCalculator();
+        var comedyCalculator = new ComedyCalculator();
+        int tragedyAmount = tragedyCalculator.CalculateAmount(play, performance);
+        int comedyAmount = comedyCalculator.CalculateAmount(play, performance);
+        return tragedyAmount + comedyAmount;
+    }
+
+    public int CalculateVolumeCredits(Performance performance)
+    {
+        var tragedyCalculator = new TragedyCalculator();
+        var comedyCalculator = new ComedyCalculator();
+        int tragedyCredits = tragedyCalculator.CalculateVolumeCredits(performance);
+        int comedyCredits = comedyCalculator.CalculateVolumeCredits(performance);
+        return tragedyCredits + comedyCredits;
+    }
+}
