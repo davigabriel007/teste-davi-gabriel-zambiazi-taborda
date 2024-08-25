bcaa47412b7f1041e8a515d6022b3ce6019000e3
Davi Gabriel Zambiazi Taborda <davi.taborda07@gmail.com>
2024-08-25 17:05:48 -0300

    Adicionado teste para o formato XML

--- TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -66,4 +66,40 @@ public class StatementPrinterTests
 
         Approvals.Verify(result);
     }
+
+    public class XmlStatementFormatterTests
+    {
+        [Fact]
+        public void TestXmlStatementExample()
+        {
+            var plays = new Dictionary<string, Play>
+        {
+            { "hamlet", new Play("Hamlet", 4024, new TragedyCalculator()) },
+            { "as-like", new Play("As You Like It", 2670, new ComedyCalculator()) },
+            { "othello", new Play("Othello", 3560, new TragedyCalculator()) },
+            { "henry-v", new Play("Henry V", 3227, new HistoryCalculator()) },
+            { "john", new Play("King John", 2648, new HistoryCalculator()) }
+        };
+
+            Invoice invoice = new Invoice(
+                "BigCo",
+                new List<Performance>
+                {
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("othello", 40),
+                new Performance("henry-v", 20),
+                new Performance("john", 39),
+                new Performance("henry-v", 20)
+                }
+            );
+
+            var formatter = new XmlStatementFormatter();
+            var statementPrinter = new StatementPrinter(formatter);
+            var result = statementPrinter.Print(invoice, plays);
+
+            Console.WriteLine(result); // Inspecionar o XML
+        }
+    }
+
 }
